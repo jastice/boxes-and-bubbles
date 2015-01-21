e19e2e0dd1019aa43f1e07846f20d2eb812cfed0
Hassan Hayat <hassan.hayat7@gmail.com>
2015-01-21 00:09:48 +0000

    Changed from List to Array and elm-linear-algebra
    Where appropriate, inside the engine, lists were replaced by arrays and
    the previously defined Vec2 type was replaced by elm-linear-algebra's
    Vec2.
    
    No algorithmic changes were made to the engine.
    
    Result:
    
    * 2x performance gain for the example
    * new instabilities uncovered/introduced by the change
    
    Todo :
    
    * Reduce reliance on pattern matching which causes numerous useless
    conversions to tuples and back
    
    * Find cause of stability issues

--- src/BoxesAndBubblesBodies.elm
+++ src/BoxesAndBubblesBodies.elm
@@ -1,20 +1,21 @@
 module BoxesAndBubblesBodies (Body, Shape(..)) where
 {-| # Boxes and Bubbles Bodies.
-Defines bodies as used by the Boxes and Bubbles engine. You will need these data types to 
-display and modify bodies being calculated. For creating them, you may prefer the constructor 
+Defines bodies as used by the Boxes and Bubbles engine. You will need these data types to
+display and modify bodies being calculated. For creating them, you may prefer the constructor
 functions in the BoxesAndBubbles module.
 
 @docs Body, Shape
 
 -}
 
-import Math2D (Vec2)
+--import Math2D (Vec2)
+import Math.Vector2 (..)
 
 {-| A rigid body in the Boxes and Bubbles universe, as used internally by the engine.
 Mass is stored as inverse, because it is more convenient for calculation.
 
 Type parameter `a` can be used to extend bodies with arbitrary other information used
-by your application. For example: label, hit points, an object type ADT, or more low-level, 
+by your application. For example: label, hit points, an object type ADT, or more low-level,
 an id used to associate the body with arbitrary other data via a Dict.
 -}
 type alias Body a = { a |
@@ -25,12 +26,12 @@ type alias Body a = { a |
   shape: Shape
 }
 
-{-| Shape data for a body. 
+{-| Shape data for a body.
 A bubble is defined by its radius.
 A box is defined by its extents (half-width/half-height from the center).
 We use half-lengths because that's what is convenient for calculation, and it's most consistent
 with using radius for circles.
 -}
-type Shape = 
+type Shape =
     Box Vec2 -- vector of extents (half-widths)
   | Bubble Float -- radius
--- src/Example.elm
+++ src/Example.elm
@@ -1,8 +1,10 @@
 import BoxesAndBubblesBodies (..)
 import BoxesAndBubbles (..)
-import Math2D (mul2)
-import List (map)
-import Graphics.Collage (..)
+--import Math2D (mul2)
+--import List (map)
+import Array (..)
+import Math.Vector2 (vec2, toTuple, scale)
+import Graphics.Collage (Form, move, filled, segment, traced, solid, toForm, group, circle, outlined, rect, collage)
 import Graphics.Element (..)
 import Color (..)
 import Text (fromString, centered)
@@ -13,55 +15,57 @@ import String
 inf = 1/0 -- infinity, hell yeah
 e0 = 0.8 -- default restitution coefficient
 
--- box: (w,h) pos velocity density restitution 
+-- box: (w,h) pos velocity density restitution
 -- bubble: radius pos velocity density restitution
 
-someBodies = [ 
-  bubble 30 1 e0(-80,0) (1.5,0),
-  bubble 70 inf 0 (80,0) (0,0) ,
-  bubble 40 1 e0 (0,200) (0.4,-3.0),
-  bubble 80 0.1 e0 (300,-280) (-2,1),
-  bubble 15 5 0.4 (300,300) (-4,-3),
-  bubble 40 1 e0 (200,200) (-5,-1),
-  box (100,100) 1 e0 (300,0) (0,0),
-  box (20,20) 1 e0 (-200,0) (3,0),
-  box (20,40) 1 e0 (200,-200) (-1,-1)
-  ] ++ bounds (750,750) 100 e0 (0,0)
+someBodies = fromList [
+  bubble 30 1 e0 (vec2 -80 0) (vec2 1.5 0),
+  bubble 70 inf 0 (vec2 80 0) (vec2 0 0) ,
+  bubble 40 1 e0 (vec2 0 200) (vec2 0.4 -3.0),
+  bubble 80 0.1 e0 (vec2 300 -280) (vec2 -2 1),
+  bubble 15 5 0.4 (vec2 300 300) (vec2 -4 -3),
+  bubble 40 1 e0 (vec2 200 200) (vec2 -5 -1),
+  box (vec2 100 100) 1 e0 (vec2 300 0) (vec2 0 0),
+  box (vec2 20 20) 1 e0 (vec2 -200 0) (vec2 3 0),
+  box (vec2 20 40) 1 e0 (vec2 200 -200) (vec2 -1 -1)
+  ] `append` bounds (vec2 750 750) 100 e0 (vec2 0 0)
 
 -- we'll just compute the label from the data in the body
-bodyLabel restitution inverseMass = 
-  ["e = ", toString restitution, "\nm = ", toString (round (1/inverseMass))] |> String.concat
+bodyLabel restitution inverseMass =
+  ["e = ", toString restitution, "\nm = ", toString (round (1 / inverseMass))] |> String.concat
 
 -- and attach it to all the bodies
 labeledBodies = map (\b -> { b | label = bodyLabel b.restitution b.inverseMass }) someBodies
 
 -- why yes, it draws a body with label. Or creates the Element, rather
-drawBody {pos,velocity,inverseMass,restitution,shape,label} = 
-  let veloLine = segment (0,0) (mul2 velocity 5) |> traced (solid red)
-      info = label |> fromString |> centered |> toForm 
+drawBody : Body {a | label : String} -> Form
+drawBody {pos,velocity,inverseMass,restitution,shape,label} =
+  let veloLine = segment (0,0) (toTuple <| scale 5 velocity) |> traced (solid red)
+      info = label |> fromString |> centered |> toForm
 
       ready = case shape of
         Bubble radius ->
-          group [ 
+          group [
             circle radius |> outlined (solid black),
             info |> move (0,radius+16),
             veloLine
             ]
-        Box extents -> 
-          let (w,h) = extents
+        Box extents ->
+          let (w,h) = toTuple extents
           in group [
-            rect (w*2) (h*2) |> outlined (solid black),
-            info |> move (0,h+16),
-            veloLine            
-          ] 
-  in move pos ready  
+            rect (w * 2) (h * 2) |> outlined (solid black),
+            info |> move (0, h + 16),
+            veloLine
+          ]
+  in move (toTuple pos) ready
 
-scene bodies = collage 800 800 <| map drawBody bodies 
+scene : Array (Body {a | label : String}) -> Element
+scene bodies = collage 800 800 <| toList <| map drawBody bodies
 
 -- different force functions to experiment with
-constgravity t = ((0,-0.2), (0,0)) -- constant downward gravity
-sinforce t = ((sin <| radians (t/1000)) * 50, 0) -- sinuoidal sideways force
-counterforces t = ((0,-0.01), (0, t/1000)) -- slowly accellerating upward drift
+constgravity t = ((vec2 0 -0.2), (vec2 0 0)) -- constant downward gravity
+sinforce t = (vec2 ((sin <| radians (t/1000)) * 50) 0) -- sinuoidal sideways force
+counterforces t = ((vec2 0 -0.01), (vec2 0 (t/1000))) -- slowly accellerating upward drift
 
 tick = constgravity <~ foldp (+) 0 (fps 40)
 
